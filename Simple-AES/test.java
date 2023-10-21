02598f63b538bba93dd4c969a6b8d39a117dcd17
leiberber <139038523+leiberber@users.noreply.github.com>
2023-10-21 23:32:02 +0800

    Add files via upload

---
+++ Simple-AES/test.java
@@ -0,0 +1,23 @@
+public class test {
+    public static void main(String[] args) {
+        // //测试S-BOX
+        // int a = 0x8A;
+        // int b;
+        // b = S_Box.s_box(a);
+        // System.out.println(Integer.toHexString(b));
+
+        //测试密钥扩展
+        int Key = 0b0010110101010101; // 16位原始密钥2D55,测试出来前四个w都对,不知道错在哪？
+        System.out.println("原始密钥: " + Integer.toHexString(Key));
+        int[] w = Key_Expansion.key_Expansion(Key);
+        
+        for (int i = 0; i < w.length; i++) {
+            //roundKey不够8位补0
+            String wk = Integer.toBinaryString(w[i]);
+            while (wk.length() < 8) {
+                wk = "0" + wk;
+            }
+            System.out.println("w" + i + ": " + wk);
+        }
+    }
+}
